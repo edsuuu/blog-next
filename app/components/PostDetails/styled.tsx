02aa2071e8bd852207b16a66198a482ad8f107a4
edsuuu <edsonteles343@gmail.com>
2024-05-22 13:46:29 -0300

    Componente PostDetails OK

--- app/components/PostDetails/styled.tsx
+++ app/components/PostDetails/styled.tsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
     margin-top: 10px;
-    font-size: 1.2rem;
+    font-size: 1.05rem;
     background-color: #8080806f;
     padding: 10px 15px;
     border-radius: 5px 25px;
     min-width: 400px;
-
+    font-family: 'Roboto', sans-serif;
     span {
         font-weight: bold;
     }
